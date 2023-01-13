ea24d4754d2fcf90506ffff1b2bccca0272e6ed9
roger-lee <1449162254@qq.com>
2023-01-13 16:14:54 +0800

    链表：实现节点

---
+++ src/main/java/linear_structure/LinkedList/Node.java
@@ -0,0 +1,13 @@
+package linear_structure.LinkedList;
+
+//每个节点
+public class Node {
+    public int data;
+    public Node next;
+
+    //构造器
+    public Node(int data){
+        this.data = data;
+    }
+
+}
